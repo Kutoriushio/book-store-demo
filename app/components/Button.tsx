1e2e0eddfcc6dcc5fd091402bc9b4d26ce695ece
Kutoriushio <328752344@qq.com>
2024-04-12 20:59:16 -0400

    feat: main page ui

---
+++ app/components/Button.tsx
@@ -0,0 +1,31 @@
+import clsx from "clsx";
+
+interface ButtonProps {
+  children?: React.ReactNode;
+  onClick?: () => void;
+  normal?: boolean;
+  danger?: boolean;
+}
+const Button: React.FC<ButtonProps> = ({
+  children,
+  onClick,
+  normal,
+  danger,
+}) => {
+  return (
+    <button
+      onClick={onClick}
+      className={clsx(
+        `flex justify-center rounded-md px-3 py-2 text-sm font-semibold focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2
+      `,
+        danger &&
+          "bg-rose-500 hover:bg-rose-600 focus-visible:outline-rose-600",
+        normal && "bg-sky-500 hover:bg-sky-600 focus-visible:outline-sky-600"
+      )}
+    >
+      {children}
+    </button>
+  );
+};
+
+export default Button;
